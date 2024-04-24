ae01dc0b2a36eaf372d67c10e647f548d39cb40d
Preslav Ivanchev <fuwclash@outlook.com>
2024-04-24 21:39:30 +0300

    feat(translation): Translate post formatted date

--- utils/get-formatted-post-date.util.ts
+++ utils/get-formatted-post-date.util.ts
@@ -1,7 +1,25 @@
 import { formatDistanceStrict, getTime } from "date-fns";
 
+import { getDictionary, locale } from "@/configs/i18n.config";
+
+const dict = getDictionary(locale).dates;
+
+type TimePeriod = keyof Omit<typeof dict, "ago">;
+
 export const getFormattedPostDate = (dateIso: string) => {
   const dateTimestamp = getTime(new Date(dateIso));
 
-  return `${formatDistanceStrict(dateTimestamp, Date.now())} ago`;
+  const formattedDate = `${formatDistanceStrict(dateTimestamp, Date.now())} ago`;
+
+  const [num, timePeriod, suffix] = formattedDate.split(" ");
+
+  let translatedDate = `${num} ${dict[timePeriod as TimePeriod]}`;
+
+  if (locale === "bg") {
+    translatedDate = `${dict[suffix as "ago"]} ${translatedDate}`;
+  } else {
+    translatedDate = `${translatedDate} ${suffix}`;
+  }
+
+  return translatedDate;
 };
